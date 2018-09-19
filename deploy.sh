7114ed5a0047548ee63c60807fc6e5adc159ffd0
Evgeny Levkov <levkov@levkov.net>
2018-09-19 13:18:33 +0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Please provide release name'
+    exit 0
+else
+    now="$(date +'%H-%M-%d-%m-%Y')"
+    docker build -t zabbix .
+    docker tag vault levkov/zabbix:$now
+    docker push levkov/zabbix:$now
+
+    helm upgrade --install  $1 .chart --debug --set vault_image.tag=$now --set vault_exporter_image.tag=$now
+fi
